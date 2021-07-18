525e4e6dbce7a53462682bf96905f2a6187919d8
= <rgowtham2910@gmail.com>
2021-07-18 06:58:09 +0000

    Updates made, login logout

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -25,12 +25,22 @@ def contact(request):
     return render(request, 'djangoapp/contact.html', {})
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST('username')
+        password = request.POST('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:login')
+        else:
+            return redirect('djangoapp:index')
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
